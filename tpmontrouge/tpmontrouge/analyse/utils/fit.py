8862e078581bdde6cb99f886d79866856cf91752
Pierre Cladé <pierre.clade@spectro.jussieu.fr>
2018-04-23 15:01:13 +0200

    Remove the fit done in a subprocess

--- tpmontrouge/tpmontrouge/analyse/utils/fit.py
+++ tpmontrouge/tpmontrouge/analyse/utils/fit.py
@@ -1,6 +1,3 @@
-from multiprocessing import Process, Queue
-import queue
-
 from scipy.optimize import curve_fit, fmin
 from numpy import pi, sin, exp, array, linspace
 
@@ -22,7 +19,7 @@ def find_frequency(t, y):
     return fmin(f_to_optimize, Tfreq[k_opt], disp=0)
 
 
-def _fit_sinusoid(t, y, freq=None, postfix='', queue=None):
+def fit_sinusoid(t, y, freq=None, postfix=''):
     """ Fit by a sinusoid. Return a positive amplitude
 
     Fit the signal y(t) by : 
@@ -35,24 +32,12 @@ def _fit_sinusoid(t, y, freq=None, postfix='', queue=None):
     phase = 0
     offset = y.mean()
     p = offset, amplitude, freq, phase
-    try:
-        popt, _ = curve_fit(sinusoid, t, y, p)
-    except RuntimeError:
-        return dict(zip(['offset', 'amplitude', 'frequency', 'phase'], p))
+    popt, _ = curve_fit(sinusoid, t, y, p)
     out = dict(zip(['offset', 'amplitude', 'frequency', 'phase'], popt))
     if out['amplitude']<0:
         out['amplitude'] = -out['amplitude']
         out['phase'] = (out['phase'] + pi)%(2*pi)
-    output = {key+postfix:val for key,val in out.items()}
-    if queue is not None:
-        queue.put(output)
-        return
-    return output
+    return {key+postfix:val for key,val in out.items()}
 
-def fit_sinusoid(t, y, freq=None, postfix=''):
-    q = Queue()
-    proc = Process(target=_fit_sinusoid, args=(t, y, freq, postfix, q))
-    proc.start()
-    res = q.get(timeout=5)
-    proc.join()
-    return res
+
+
